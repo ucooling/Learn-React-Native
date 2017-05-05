56a7b2fd1e8f142e41bf552ab96556d67e877b0d
ucooling <mlb0903@163.com>
2017-05-05 11:41:27 +0800

    使用react-redux实现一个 +1 -1 归零的计数器

--- index.ios.js
+++ index.ios.js
@@ -1,27 +1,4 @@
-import React, { Component } from 'react';
-import { AppRegistry, ListView, Text, View } from 'react-native';
+import { AppRegistry } from 'react-native';
+import App from './src/app';
 
-class AwesomeProject extends Component {
-    // 初始化模拟数据
-    constructor(props) {
-        super(props);
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        this.state = {
-            dataSource: ds.cloneWithRows([
-                'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
-            ])
-        };
-    }
-    render() {
-        return (
-            <View style={{flex: 1, paddingTop: 22}}>
-                <ListView
-                    dataSource={this.state.dataSource}
-                    renderRow={(rowData) => <Text>{rowData}</Text>}
-                />
-            </View>
-        );
-    }
-}
-
-AppRegistry.registerComponent('AwesomeProject', ()=> AwesomeProject);
+AppRegistry.registerComponent('AwesomeProject', ()=> App);
